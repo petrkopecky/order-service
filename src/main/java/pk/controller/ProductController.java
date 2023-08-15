5eb175da047eb59f241537e330ef45796633cd3b
petrkopecky <petr.kopecky@gmail.com>
2023-08-15 10:34:29 +0200

    call product service, hostname as param

--- src/main/java/pk/controller/ProductController.java
+++ src/main/java/pk/controller/ProductController.java
@@ -1,17 +1,24 @@
 package pk.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import pk.mapperDto.ProductDto;
+import pk.service.OrderService;
 
 @RestController
 public class ProductController {
+    Logger logger = LoggerFactory.getLogger(ProductController.class);
     //refactor resource_url to application.yml
     //private static final String RESOURCE_URL = "http://172.24.0.2:8080/products";
 
-    private static final String RESOURCE_URL = "http://product-service-web-1:8080/products";
+    @Value("${application.product-service-url}")
+    private String productServiceUrl;
+
     private RestTemplate restTemplate;
 
     public ProductController(RestTemplateBuilder restTemplateBuilder) {
@@ -19,6 +26,7 @@ public class ProductController {
     }
 
     public ProductDto fetchProductById(long id) {
-        return restTemplate.getForObject(RESOURCE_URL+"/"+String.valueOf(id), ProductDto.class);
+
+        return restTemplate.getForObject(productServiceUrl+"/"+String.valueOf(id), ProductDto.class);
     }
 }
